a29041f4892e29a8454a3da0b99872e8f44cd14b
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-02-07 22:29:14 +0100

    a trial of cap tasks for db preparation

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,8 +20,19 @@ role :db,  "sazabi.pl"
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
+before "deploy:restart", "prepare:db"
+
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+
+namespace :prepare do
+  task :db do
+    run "#{deploy_to}/current/create_db.rb"
+  end
+  task :db_load do
+    run "#{deploy_to}/current/fixtures.rb"
+  end
+end
 
 # If you are using Passenger mod_rails uncomment this:
 # namespace :deploy do
